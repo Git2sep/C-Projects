964b53df1732bf6c1219c6672334b24567a9fdae
Thomas12397 <thomasclh1997@gmail.com>
2019-08-24 15:30:26 -0500

    updating PC repo

--- the-c-programming-language/ch2-htoi-2-3.c
+++ the-c-programming-language/ch2-htoi-2-3.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <math.h>
 #include <string.h>
+#include <limits.h>
 
 int htoi(char *s);
 
@@ -8,28 +9,35 @@ int main() {
 
 	char *hex = "0x5AF43";
 
-	printf("Convert the hexadecimal value %s to decimal:\n %d\n", hex, htoi(hex));
+	printf("Convert the hexadecimal value \"%s\" to decimal:\n%d\n", hex, htoi(hex));
 
 	return 0;
 }
 
 int htoi(char *s) {
 
-	int res = 0, i = 0;
-	double power = 0.0, exp = 0.0;
+	int i, val;
 
-	for (i = 0; s[i] != '\0'; ++i) {
-		if (s[i] >= '0' && s[i] <= '9') {
-			exp = (double) (strlen(s) - i);
-			power = pow(16.0, exp);
-			res = 16 * s[i] + ('0' - '9')));
-		}
-		else if (s[i] >= 'A' && s[i] <= 'F') {
-			exp = (double) (strlen(s) - i);
-			power = pow(16.0, exp);
-			res = ( (s[i] + (('A' - 'F') * 2)) * power); 
-		}
-	}
-
-	return res;
+	if(s[i] == '0' && (s[i+1]=='x' || s[i+1]=='X')) 
+		i += 2;
+     
+    while(s[i] != '\0') {
+       
+       if(val > UINT_MAX) 
+       		return 0;
+       else if(s[i] >= '0' && s[i] <='9') {
+          	val = val * 16 + s[i] - '0';
+       }
+       else if(s[i]>='A' && s[i] <='F') {
+          	val = val * 16 + s[i] - 'A' + 10;
+       }
+       else if(s[i]>='a' && s[i] <='f') {
+          	val = val * 16 + s[i] - 'a' + 10;
+       }
+       else 
+       		return 0;
+        
+       i++;
+    }
+    return val;
 }

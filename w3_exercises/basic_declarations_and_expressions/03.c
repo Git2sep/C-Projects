25bcf5007898664d4ffd123cc2d06f2c78e1990d
Thomas12397 <thomasclh1997@gmail.com>
2019-08-27 16:29:05 -0500

    Working on exercises in w3schools for C

---
+++ w3_exercises/basic_declarations_and_expressions/03.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+  * Print a block F
+  */
+int main() {
+
+  printf("   ######\n ##      ##\n#\n#\n#\n#\n ##      ##\n   #####\n");
+  return 0;
+}

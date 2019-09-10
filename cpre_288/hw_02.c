1039a8722848107c7537ad7de5961915c59d9e96
Thomas12397 <thomasclh1997@gmail.com>
2019-09-10 10:17:37 -0500

    288 hw02

--- cpre_288/hw_02.c
+++ cpre_288/hw_02.c
@@ -2,6 +2,28 @@
 
 int main() {
 
-  printf("Hello, my name is Thomas Haddy!\n");
+  struct more_compound {
+	char *name;
+	int  age;
+	short pay;
+	long *height;
+
+union {
+	char short_id;
+	int  normal_id;
+	char my_id;
+} id;
+
+
+	union {
+	char *text_data;
+	int  *numeric_data;
+	long l_numeric_data;
+} data;
+};
+
+
+
+  printf("%ld\n", sizeof(struct more_compound));
   return 0;
 }

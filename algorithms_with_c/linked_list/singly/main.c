e0eaec39fca590f088e0fa476b7df286a16e844a
Thomas12397 <thomasclh1997@gmail.com>
2020-03-23 13:41:59 -0500

    Finished singly data strcuture. Moving to testing

---
+++ algorithms_with_c/linked_list/singly/main.c
@@ -0,0 +1,7 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "singly_linked_list.h"
+
+int main(void) {
+  return 0;
+}
---
+++ algorithms_with_c/linked_list/singly/singly_linked_list.c
@@ -0,0 +1,93 @@
+#include <stdlib.h>
+#include <string.h>
+
+#include "singly_linked_list.h"
+
+void list_init(List *list, void (*destroy)(void *data)) {
+
+  list->size = 0;
+  list->destroy = destroy;
+  list->head = NULL;
+  list->tail = NULL;
+  return;
+}
+
+void list_destroy(List *list) {
+
+  void *data;
+  while (list_size(list) > 0) {
+    if (list_rem_next(list, NULL, (void **)&data) == 0 && list->destroy != NULL) {
+      list->destroy(data);
+    }
+  }
+
+  memset(list,0,sizeof(list));
+  return;
+}
+
+int list_ins_next(List *list, Node *element, const void *data) {
+
+  Node *new_element;
+
+  if ((new_element = (Node *)malloc(sizeof(Node))) == NULL) {
+    return -1;
+  }
+
+  new_element->data = (void *)data;
+
+  if (element == NULL) {
+
+    if (list_size(list) == 0) {
+      list->tail = new_element;
+    }
+    new_element->next = list->head;
+    list->head = new_element;
+  }
+  else {
+
+    if (element->next == NULL) {
+      list->tail = new_element;
+    }
+    new_element->next = element->next;
+    element->next = new_element;
+  }
+
+  list->size++;
+  return 0;
+}
+
+int list_rem_next(List *list, Node *element, void **data) {
+
+  Node *old_element;
+
+  if (list_size(list) == 0) {
+    return -1;
+  }
+
+  if (element == NULL) {
+    *data = list->head->data;
+    old_element = list->head;
+    list->head = list->head->next;
+
+    if (list_size(list) == 1) {
+      list->tail = NULL;
+    }
+  }
+  else {
+    if (element->next == NULL) {
+      return -1;
+    }
+
+    *data = element->next->data;
+    old_element = element->next;
+    element->next = element->next->next;
+
+    if (element->next == NULL){
+      list->tail = element;
+    }
+  }
+
+  free(old_element);
+  list->size--;
+  return 0;
+}
